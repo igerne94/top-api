7565972950aab36e76bbf7b4f8b519a71f9429c4
igerne94 <igerne@gmail.com>
2023-10-15 18:16:49 +0200

    Modules: auth, product, review, top-page; defined Models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ProductModule } from './product/product.module';
+import { RewievModule } from './rewiev/rewiev.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TopPageModule, ProductModule, RewievModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/rewiev/rewiev.model.ts
@@ -0,0 +1,7 @@
+export class RewievModel {
+  name: string;
+  title: string;
+  description: string;
+  rating: number;
+  createdAt: Date;
+}
