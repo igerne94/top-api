85e3dece9bcfc66de520d1427751250920584d7f
igerne94 <igerne@gmail.com>
2023-10-31 18:22:25 +0100

    connected mongodb; mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { RewievModule } from './rewiev/rewiev.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/test'),
     ConfigModule.forRoot(),
     AuthModule,
     TopPageModule,
