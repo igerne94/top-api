b0ca062ce8b3ceb297f73d851333a231134c6b6b
Hanna Khonskaya <48218270+igerne94@users.noreply.github.com>
2023-11-09 19:16:52 +0100

    Authorization (#7)
    * auth model--> user model; 
    
    * auth service; 
    
    * validation Auth dto; bcryptjs;
    
    * auth: register user controller
    
    * authorization and gen access token
    
    * jwt strategy and guard
    
    ---------
    
    Co-authored-by: igerne94 <igerne@gmail.com>

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModel, AuthModelSchema } from './auth.model';
-import { ConfigService } from '@nestjs/config';
+import { UserModel, UserModelSchema } from './user.model';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { getJWTConfig } from 'src/configs/jwt.config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
   imports: [
+    // TODO: is MongooseModule imported needed?
     MongooseModule.forFeature([
       {
-        name: AuthModel.name,
-        schema: AuthModelSchema,
+        name: UserModel.name,
+        schema: UserModelSchema,
       },
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTConfig,
+    }),
+    ConfigModule,
+    PassportModule,
   ],
-  providers: [ConfigService],
+  providers: [ConfigService, AuthService, JwtStrategy],
 })
 export class AuthModule {}
