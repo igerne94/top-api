389c533e24208bb94e36b4a6a59406d4967208e5
igerne94 <igerne@gmail.com>
2023-11-15 20:54:01 +0100

    prod service

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -4,6 +4,7 @@ import {
   Delete,
   Get,
   HttpCode,
+  NotFoundException,
   Param,
   Patch,
   Post,
@@ -11,28 +12,44 @@ import {
 import { ProductModel } from './product.model';
 import { FindProductDto } from './dto/find-product.dto';
 import { CreateProductDto } from './dto/create-product.dto';
+import { ProductService } from './product.service';
+import { PRODUCT_NOT_FOUND_ERROR } from './product.constants';
 
 @Controller('product')
 export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
   @Post('create')
   async create(@Body() dto: CreateProductDto) {
-    console.log(dto);
+    return this.productService.create(dto);
   }
 
   @Get(':id')
   async get(@Param('id') id: string) {
-    console.log(id);
+    const product = await this.productService.findById(id);
+    if (!product) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+    }
+    return product;
   }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {
-    console.log(id);
+    const deleted = await this.productService.deleteById(id);
+    if (!deleted) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+    }
+    return deleted;
   }
 
   @Patch(':id')
   async patch(@Param('id') id: string, @Body() dto: ProductModel) {
-    console.log(id);
-    console.log(dto);
+    // why not CreateProductDto ?
+    const updated = await this.productService.updateById(id, dto);
+    if (!updated) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+    }
+    return updated;
   }
 
   @HttpCode(200)
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ProductModel } from './product.model';
+import { Model } from 'mongoose';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel(ProductModel.name)
+    private readonly productModel: Model<ProductModel>,
+  ) {}
+
+  async create(dto: CreateProductDto): Promise<ProductModel> {
+    return this.productModel.create(dto);
+  }
+
+  async findById(id: string): Promise<ProductModel> {
+    return this.productModel.findById(id).exec();
+  }
+
+  async deleteById(id: string): Promise<ProductModel> {
+    return this.productModel.findByIdAndDelete(id).exec();
+  }
+
+  async updateById(id: string, dto: ProductModel): Promise<ProductModel> {
+    return this.productModel.findByIdAndUpdate(id, dto, { new: true }).exec();
+  }
+}
