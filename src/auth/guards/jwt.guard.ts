c86be4fd50a92677b46a0448eb32c026a1baa838
igerne94 <igerne@gmail.com>
2024-01-18 19:01:54 +0100

    fixed bug, added comments

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,3 +1,10 @@
 import { AuthGuard } from '@nestjs/passport';
 
+/**
+ * It automatically uses JwtStrategy
+ * to validate the JWT of incoming requests.
+ * If the token is valid,
+ * the request is allowed to proceed;
+ * if not, it throws an exception.
+ */
 export class JwtAuthGuard extends AuthGuard('jwt') {}
