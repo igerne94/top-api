c139fb03641830112e93b498fa23044430541e92
igerne94 <igerne@gmail.com>
2024-04-28 00:04:52 +0200

    included environment for top-api img; networls; simple get of MONGODB_URI when mongo local

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -13,15 +13,7 @@ export const getMongoConfig = async (
 };
 
 const getMongoString = (configService: ConfigService): string | null => {
-  return (
-    configService.get<string>('MONGO_BASE') +
-    '://' +
-    configService.get<string>('MONGO_HOST') +
-    ':' +
-    configService.get<string>('MONGO_PORT') +
-    '/' +
-    configService.get<string>('MONGO_DB')
-  );
+  return configService.get<string>('MONGODB_URI');
 };
 
 const getMongoOptions = () => {
